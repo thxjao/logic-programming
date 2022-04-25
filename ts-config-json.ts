71634792f63454c9bba8257d99de7e458005adcd
Joao Oliveira <thxjao.dev@gmail.com>
2022-04-25 19:54:53 -0300

    docs: started learning tsconfig file

---
+++ ts-config-json.ts
@@ -0,0 +1,19 @@
+// What is the tsconfig.json file, and for what it helps us in our projects?
+
+// It's important that every project that uses typescript must have the tsconfig.json
+
+/* {
+    "compilerOptions": {
+        "lib": ["es2015"], // What javascript type is gonna be runned 
+        "module": "commonjs",
+        "outDir": "dist", // Where the transpiled code have to be generated
+        "sourceMap": true, 
+        "strict": true, // Recommended to set to true, this will keep your files correct
+        "target": es2015  // What typescript version TSC is going to be transpiled
+    },
+    "include": {
+        "src" // Basic configure where to run the typescript compiler
+    }
+} */
+
+// Search -  What is transpilation
