dbe0720ceebfa04bca3f66043c00676f30b52ece
Joao Oliveira <thxjao.dev@gmail.com>
2022-04-26 18:10:24 -0300

    feat: why use typescript

---
+++ Typescript/notes/why-use-typescript.ts
@@ -0,0 +1,8 @@
+// Typescript is the javascript's future
+
+// With Typescript we can use the ES6, ES7, ES8 and even ES2020.
+
+// Strongly supports POO and FRP.
+
+// Has static typing, that help us to debbug the code more easly
+// to read and organized.
